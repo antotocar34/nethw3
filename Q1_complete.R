72493f0e01016593ddf35b09bf50c3aff04040ab
Leon Karreman <70812084+enigma-c@users.noreply.github.com>
2022-02-21 17:21:53 +0100

    Update Q1_complete.R

--- Q1_complete.R
+++ Q1_complete.R
@@ -14,22 +14,21 @@ memb <- sample(1:2,20, replace = TRUE)
 graph <- sample_ppm(memb, 0.14, 0.06)
 
 
-graph %>%
+lesmis_graph %>%
   ggraph() +
   geom_edge_link0(colour = 'grey75') +
-  geom_node_point(aes(col = factor(memb)), show.legend = FALSE) +
+  geom_node_point( show.legend = FALSE) +
   scale_colour_brewer(palette = 'Set1') +
-  theme_void()+ggtitle('p=0.14 and q=0.06')
+  theme_void()+ggtitle('Les Miserables Network with 76 Vertices and 250 Edges ')
 
 
 #load graph
 
 load("C:\\Users\\karre\\OneDrive\\Documentos\\GitHub\\hw_3\\data\\lesmis_graph.RData")
 
-V(lesmis_graph)
 
 seed=sample(1:20,2)
-#the SI_model function simulates the data generating process, the network creation and the infection process, for different infection probabilities, n, d, e and c
+#the diffusion function simulates a simple diffusion process that follows the model in the excercise. 
 diffusion <- function(graph, lambda,seed){
   #create a vector with the vertices
   vertices<-V(graph)
@@ -86,3 +85,38 @@ pairwise_search<- function(graph, lambda) {
 }
 result_lambda2 <-pairwise_search(lesmis_graph, 2)
 save(result_lambda2, file="C:\\Users\\karre\\OneDrive\\Documentos\\GitHub\\hw_3\\data\\lambda_2")
+
+load("C:\\Users\\karre\\OneDrive\\Documentos\\GitHub\\hw_3\\data\\lambda_2")
+
+optimal_pair=result_lambda2[which.max(result_lambda2$X3),]
+pair=c(41,42)
+
+g2 <- induced.subgraph(graph=lesmis_graph,vids=pair)
+lesmis_graph %>%
+  ggraph() +
+  geom_edge_link0(colour = 'grey75') +
+  geom_node_point( show.legend = FALSE) +
+  geom_node_point(aes(colour="red"),g2,show.legend = FALSE)+
+  scale_colour_brewer(palette = 'Set1') +
+  theme_void()+ggtitle('Les Miserables Network with 76 Vertices and 250 Edges ')
+#result, pair= 41 and 42 with an information rate of 0.2122807
+#the following function performs the same diffusion simulation but calculates the mean information rate of 1000 seed-pairs sampled at random
+random_search<- function(graph, lambda) {
+ result=c()
+ n=gorder(graph)
+   for (i in 1:500) {
+    seed=sample(1:n,2)
+    information_measure=diffusion(graph, lambda, seed)
+    result=c(result, information_measure)
+    
+  } 
+ 
+  
+  return(mean(result))
+}
+
+# run the random search with lambda=1 and lambda=2
+random_search(lesmis_graph, 1)
+# result=0.1959211
+random_search(lesmis_graph, 2)
+#result= 0.03736842
